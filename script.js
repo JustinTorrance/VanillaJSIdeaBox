d94c01312679c673e387c5c0a53be8c15168b942
Justin Torrance <justin.torrance91@gmail.com>
2018-12-13 13:35:21 -0700

    Add price input

--- script.js
+++ script.js
@@ -4,27 +4,31 @@ document.querySelector('.card-container').addEventListener('click', removeGift)
 function handleSubmit(e) {
   e.preventDefault()
   const title = document.querySelector('.title-input').value
-  const recipient = document.querySelector('.recipient').value
+  const recipient = document.querySelector('.recipient-input').value
+  const price = document.querySelector('.price-input').value
   const notes = document.querySelector('.notes-input').value
-  const newCard = new CardConstructor(title, recipient, notes)
-  cardHTML(newCard)
+  const newCard = new CardConstructor(title, recipient, notes, price)
+  generateHTML(newCard)
 }
 
-function cardHTML(card) {
+function generateHTML(card) {
   document.querySelector('.card-container').innerHTML += 
     `<article class="card">
-      <h3>${card.title}</h3>
-      <h5>For ${card.recipient}</h5>
-      <p>${card.notes}</p>
+      <h3 class="title" >${card.title}</h3>
+      <h5 class="recipient">for ${card.recipient}</h5>
+      <h5 class="price">price: ${card.price}</h5>
+      <p class="notes">${card.notes}</p>
+      <p class="quality">Will they love it? ${card.quality}</p>
       <button class="remove-btn">Remove</button>
     </article>`
 }
 
-function CardConstructor(title, recipient, notes) {
+function CardConstructor(title, recipient, notes, price) {
   this.id = Date.now()
   this.title = title
-  this.notes = notes
   this.recipient = recipient
+  this.price = price
+  this.notes = notes
   this.quality = 'Meh'
 }
 
